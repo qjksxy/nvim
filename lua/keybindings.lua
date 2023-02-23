b058505e0b1d47139567aea0c358d4cee6a77e30
piner <qjksxy@163.com>
2023-02-23 22:31:56 +0800

    feat: :tada: 初始化仓库

---
+++ lua/keybindings.lua
@@ -0,0 +1,52 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local map = vim.api.nvim_set_keymap
+-- 复用 opt 参数
+local opt = {noremap = true, silent = true }
+-- 取消 s 默认功能
+map("n", "s", "", opt)
+-- windows 分屏快捷键
+map("n", "sv", ":vsp<CR>", opt)
+map("n", "sh", ":sp<CR>", opt)
+-- 关闭当前
+map("n", "sc", "<C-w>c", opt)
+-- 关闭其他
+map("n", "so", "<C-w>o", opt)
+-- Alt + hjkl  窗口之间跳转
+map("n", "ssh", "<C-w>h", opt)
+map("n", "ssj", "<C-w>j", opt)
+map("n", "ssk", "<C-w>k", opt)
+map("n", "ssl", "<C-w>l", opt)
+-- 左右比例控制
+map("n", "<C-Left>", ":vertical resize -2<CR>", opt)
+map("n", "<C-Right>", ":vertical resize +2<CR>", opt)
+-- 上下比例
+map("n", "<C-Down>", ":resize +2<CR>", opt)
+map("n", "<C-Up>", ":resize -2<CR>", opt)
+-- 等比例
+map("n", "s=", "<C-w>=", opt)
+-- visual模式下缩进代码
+map("v", "<", "<gv", opt)
+map("v", ">", ">gv", opt)
+-- 上下移动选中文本
+map("v", "J", ":move '>+1<CR>gv-gv", opt)
+map("v", "K", ":move '<-2<CR>gv-gv", opt)
+
+-- insert 模式下，跳到行首行尾
+map("i", "<C-h>", "<ESC>I", opt)
+map("i", "<C-l>", "<ESC>A", opt)
+-- 上下滚动浏览
+map("n", "<C-j>", "4j", opt)
+map("n", "<C-k>", "4k", opt)
+-- ctrl u / ctrl + d  只移动9行，默认移动半屏
+map("n", "<C-u>", "9k", opt)
+map("n", "<C-d>", "9j", opt)
+
+
+
+
+
+
+
+
